1a08cd7bff9ab2f609c2cad897aff2f883d6af05
Moises Alvarez <mfalvare@espol.edu.ec>
2024-07-20 17:21:23 -0500

    Stop 3 guia 18

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+ /* 1. Cargue los modelos de acuerdo con la configuración de la conexión */
+ const sequelize = require('../models/index.js').sequelize;
+ var initModels = require("../models/init-models");
+ var models = initModels( sequelize );  
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-     /* 1. Renderización de la vista crud.ejs */
-     res.render('crud');
+router.get('/', async function(req, res, next) {
+     
+      /* 3. Uso del método findAll */
+   let usersCollection = await models.users.findAll({ })
+   res.render('crud', { title: 'CRUD with users', usersArray: usersCollection });
   
 });
 
