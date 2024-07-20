ca79b5a2139a20305b4692d60fa96727a0f5d38a
Moises Alvarez <mfalvare@espol.edu.ec>
2024-07-20 17:12:49 -0500

    Stop 2 guia 18

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
+
+// Configurar el motor de vistas EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+     /* 1. Renderización de la vista crud.ejs */
+     res.render('crud');
+  
 });
 
 module.exports = router;
