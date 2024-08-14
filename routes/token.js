6d1a6256bbf764421d1f0a00fa16886e907e5977
jair-chaguay <jchaguay@espol.edu.ec>
2024-08-14 13:55:29 -0500

    Add jsonwebtoken

--- routes/token.js
+++ routes/token.js
@@ -1,8 +1,30 @@
+require('dotenv').config()
+
 var express = require("express");
 var router = express.Router();
 
+
+const sequelize = require('../models/index.js').sequelize;
+var initModels = require("../models/init-models");
+var models = initModels(sequelize);
+
+const jwt = require('jsonwebtoken');
+const { TOKEN_SECRET } = process.env;
+
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  return res.render("token", { title: "Token user" ,username:req.cookies['username']});
-  });
-  module.exports = router;
+
+  const payload = {
+    user: {
+      id: req.session.id,
+      username: req.session.username
+    },
+    role: req.session.role
+  };
+
+  const token = jwt.sign(payload, TOKEN_SECRET, { expiresIn: '1h' });
+
+  return res.render("token", { title: "Token user", username: req.cookies['username'], token } );
+});
+module.exports = router;
