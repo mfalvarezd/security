0b50e7eb3b4dbd3656dc363d733923a2793638fb
Moises Alvarez <mfalvare@espol.edu.ec>
2024-08-14 13:35:25 -0500

    logout de token

--- middleware/authorization_session.js
+++ middleware/authorization_session.js
@@ -4,13 +4,13 @@ var authorizationSession = (req, res, next) => {
     const userRole = req.session.role;
 
     if(userRole === 'user') {
-        // Redirige a la vista token si el rol es "user"
-        return res.render("token", { title: "Token user" });
+        
+        return res.render("token", { title: "Token user" ,username:req.cookies['username']});
     } else if(process.env.ALL_GRANTED.includes(userRole)) {
-        // Permite el acceso si el rol está en ALL_GRANTED
+       
         return next();
     } else {
-        // Redirige a la página principal si no está autorizado
+        
         return res.redirect("/");
     }
 }
